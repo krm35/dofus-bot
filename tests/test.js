9dc9ea4db136d34e76c25d0ee28aa0eed322cb90
krm35 <etcc@protonmail.com>
2023-08-03 22:58:04 +0200

    Remove unused code

--- tests/test.js
+++ tests/test.js
@@ -2,25 +2,20 @@ const fs = require('fs'),
     {spawn} = require('child_process'),
     {strictEqual} = require('assert');
 
-const osCommand = function (command, args) {
+const osCommand = (command, args) => {
     return new Promise(resolve => {
-        const process = spawn(command, args || []);
-        let result = "";
-        process.stdout.on('data', (data) => result += data);
+        let process = spawn(command, args || []), result = "";
+        process.stdout.on('data', data => result += data);
+        process.stderr.on('data', data => result += data);
         process.on('error', () => null);
-        process.on('close', (code) => resolve({code, result}));
+        process.on('close', code => resolve({code, result}));
     });
 };
 
 (async () => {
     const files = fs.readdirSync(".").filter(f => f.endsWith(".test.js"));
     for (const file of files) {
-        const content = fs.readFileSync("./" + file).toString();
-        if (content.indexOf('process') !== content.lastIndexOf('process')) {
-            console.log(file, 'multiple process.exit');
-            strictEqual(true, false);
-        }
-        const {code, result} = await osCommand("node", ["./" + file], true);
+        const {code, result} = await osCommand("node", ["./" + file]);
         console.log(file, result);
         strictEqual(code, 0);
     }
