be0a747ae8cfd275abf390577abe99d4cbf907fb
krm35 <etcc@protonmail.com>
2023-08-12 09:50:23 +0200

    Decrypt ticket in node for the SelectedServerDataMessage

--- p/SelectedServerDataMessage.js
+++ p/SelectedServerDataMessage.js
@@ -1,9 +1,15 @@
-const {execSync} = require('child_process'),
+const crypto = require('crypto'),
+    ByteArray = require('bytearray-node'),
     {decode} = require('../utilities');
 
 module.exports = async (s, p, {connect}) => {
     const {address, ports, ticket} = await decode(p.data);
     const socket = connect(address, ports[0]);
-    socket.ticket = execSync("java -jar " + __dirname + "/Credentials/out/artifacts/Credentials_jar/Credentials.jar \""
-        + s.aes + "\" \"" + Buffer.from(ticket).toString("hex") + "\"").toString();
+    const key = new ByteArray(Buffer.from(s.aes, "hex"));
+    const iv = new ByteArray();
+    iv.writeBytes(key, 0, 16);
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key.buffer, iv.buffer);
+    decipher.setAutoPadding(false);
+    const decrypted = decipher.update(Buffer.from(ticket), null, 'hex') + decipher.final('hex');
+    socket.ticket = Buffer.from(decrypted, 'hex').toString();
 };
